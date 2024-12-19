e27395b661f5d1c795fcf7774fd73fb4d20651ad
xhBIROhx <birorihard@gmail.com>
2024-12-19 20:53:05 +0100

    F01 feladat elkészítve

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.ConstrainedExecution;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static Random rand;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void F01(int[] tomb) {
+            for (int i = 0; i < tomb.Length; i++)
+            {   
+                tomb[i] += rand.Next(1,51);
+            }
+        }
+    }
+}
